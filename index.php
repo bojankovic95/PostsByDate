0aa4b3f6fc13e9ae59d5d5f502ef9440d180e825
bojankovic95 <68285886+bojankovic95@users.noreply.github.com>
2022-07-11 19:52:46 +0200

    Dashboard form added
    added dashboard form with fields for choosing category, start date and number of posts

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 /*
 Plugin Name: Posts By Date
@@ -13,13 +16,32 @@ add_action("admin_menu", "addMenu");
 
 function addMenu(){
     add_menu_page("Posts By Date", "Posts By Date", 4, "posts-by-date", "postsByDate");
-    add_submenu_page("posts-by-date", "Option 1", "Opstion 2", 4, "posts1", "option1");
 }
 
 function postsByDate(){
-    echo "This is the test page";
-}
+    if(isset($_POST['submit'])){
+        $category = $_POST['category'];
+        $dateStart = $_POST['dateStart'];
+        $number = $_POST['number'];
+    }
+    
+    $cats = get_categories();
+    ?>
+        <form method="post">
+        <label for="category">Choose a category:</label> <br>
+            <select id="category" name="category">
+            <option value="unset" selected disabled>-- Choose a Category --</option>
+            <?php
+            foreach ($cats as $c){
+                ?> <option value="<?php echo $c->slug; ?>"><?php echo $c->name; ?></option> <?php
+            } ?>
+            </select> <br><br>
+            <label for="dateStart">Choose start date:</label><br>
+            <input type="date" name="dateStart"><br><br>
+            <label for="number">Set number of posts to display:</label><br>
+            <input type="number" name="number" value="5"><br><br>
 
-function posts1(){
-    echo "Test subpage";
+            <input type="submit" name="submit" value="Save">
+        </form>
+    <?php
 }
